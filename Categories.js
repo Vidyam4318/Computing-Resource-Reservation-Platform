4089dd4844f405937414b0be5054d42b7b3b0048
Vidya <114570375+Vidyam4318@users.noreply.github.com>
2024-12-18 10:33:16 +0530

    Create Categories.js

---
+++ Categories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <div className="categories">
+            <h2>Choose a Category</h2>
+            <div className="category-options">
+                <button>Short Term Initiative</button>
+                <button>Long Term initiative</button>
+            </div>
+        </div>
+    );
+};
+
+export default Categories;
