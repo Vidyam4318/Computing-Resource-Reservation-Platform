85b79115ef451ae4ac5db27a742e51d6715aa237
Vidya <114570375+Vidyam4318@users.noreply.github.com>
2024-12-18 10:56:17 +0530

    Create ShortTerm.js

---
+++ ShortTerm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ShortTerm = () => {
+    return (
+        <div className="short-term">
+            <h2>Short Term Usage</h2>
+            <p>Short-term users can access our platform using:</p>
+            <ul>
+                <li>Jupyter Notebook: A web-based interactive computing platform.</li>
+                <li>PuTTY: A free SSH and Telnet client for remote access.</li>
+            </ul>
+        </div>
+    );
+};
+
+export default ShortTerm;
