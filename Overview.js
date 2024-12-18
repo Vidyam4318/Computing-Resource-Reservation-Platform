1de5f5d4f99cd15d80d5086c44dc7b11e6b1a170
Vidya <114570375+Vidyam4318@users.noreply.github.com>
2024-12-18 10:33:44 +0530

    Create Overview.js

---
+++ Overview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Overview = () => {
+    return (
+        <div className="overview-content">
+            <h2>Overview</h2>
+            <p>This is the overview content that will be displayed when you click on Overview in the navigation bar.</p>
+            <p>Feel free to customize this content as needed!</p>
+        </div>
+    );
+};
+
+export default Overview;
