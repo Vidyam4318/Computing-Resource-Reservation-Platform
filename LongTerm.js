770bc1ea90f410a54971c7e4710907d4652f0726
Vidya <114570375+Vidyam4318@users.noreply.github.com>
2024-12-18 10:51:07 +0530

    Create LongTerm.js

---
+++ LongTerm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LongTerm = () => {
+    return (
+        <div className="long-term">
+            <h2>Long Term Usage</h2>
+            <p>Long-term users can take advantage of:</p>
+            <ul>
+                <li>Jupyter Notebook: A flexible tool for research and development.</li>
+                <li>PuTTY: Reliable remote server connectivity for extended durations.</li>
+            </ul>
+        </div>
+    );
+};
+
+export default LongTerm;
